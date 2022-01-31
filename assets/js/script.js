ac1730c4234673abd16b5cabb18f9a84cb45325f
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-01-31 19:00:36 +0000

    Local storage/ Getting data for cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var weatherKey = "bba984a84c023f7dd8d62dc48cd89120"
 var cities = [];
 
 var cityFormEl = document.querySelector("#city-search-form");
@@ -7,6 +8,8 @@ var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+//Get a city weather value when entering a city name on the search form.
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -23,4 +26,29 @@ var formSubmitHandler = function(event){
     saveSearch();
     pastSearch(city);
 }
+
+//Local storage
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+    
+};
+
+//getting weather date for cities by using the API openweathermap.
+
+var getCityWeather = function(city){
+var weatherKey = "bba984a84c023f7dd8d62dc48cd89120"
+var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+fetch(apiUrl) 
+    .then(function(response) {
+        response.JSON()
+        .then(function(data) {
+            displayWeather(data,city);
+        });
+    });
+};
+
+
+
+
 
