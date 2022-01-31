99aedbb9097a4802315b7d43d67d6f7a318afae3
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-01-31 21:00:50 +0000

    Setting the UV Index display

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,5 +110,29 @@ var getUvIndex = function(lat,lon){
     //console.log(lon);
 }
 
+// setting the UV index display
+var displayUvIndex = function(index){
+    var uvIndexEl = document.createElement("div");
+    uvIndexEl.textContent = "UV Index: "
+    uvIndexEl.classList = "list-group-item"
+
+    uvIndexValue = document.createElement("span")
+    uvIndexValue.textContent = index.value
+
+    if(index.value <=2){
+        uvIndexValue.classList = "favorable"
+    }else if(index.value >2 && index.value<=8){
+        uvIndexValue.classList = "moderate "
+    }
+    else if(index.value >8){
+        uvIndexValue.classList = "severe"
+    };
+
+    uvIndexEl.appendChild(uvIndexValue);
+
+    //append index to current weather
+    weatherContainerEl.appendChild(uvIndexEl);
+}
+
 
 
