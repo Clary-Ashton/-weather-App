5793ca7d16d5b10d25ad7ad039dcbb09faff575c
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-01-31 18:28:36 +0000

    Getting value when entering a city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,20 @@ var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a city");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
